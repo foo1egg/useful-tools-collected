75ee89534ab3e8edb24a7d1f2f99bab8b1c8c5cc
a biddable man <94541432+foo1egg@users.noreply.github.com>
2022-06-05 10:15:58 +0800

    Create Website Batch Download.bat

---
+++ Website Batch Download.bat
@@ -0,0 +1,13 @@
+# the downloadlist.txt should include all the https links you need, one line after another...
+# this tools was collected from https://blog.csdn.net/domo_wang/article/details/107045785
+# double click .bat to start
+# 2022.6.4
+
+@ echo off
+setlocal enabledelayedexpansion
+for /f %%i in (YOUR_PATH\downloadlist.txt) do (
+start "YOUR_PATH\chrome.exe" "%%i"
+Ping -n 5 127.1>nul
+)
+Ping -n 60 127.1>nul
+exit
